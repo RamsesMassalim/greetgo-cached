6783b567dd8bc3a7ed6a4158acd40005264321f1
ekolpakov <ekolpakov@greet-go.com>
2021-07-02 09:53:40 +0600

    Создал модуль core

---
+++ greetgo-cached-core/src/kz/greetgo/cached/core/Dummy.java
@@ -0,0 +1,7 @@
+package kz.greetgo.cached.core;
+
+public class Dummy {
+  public void print() {
+    System.out.println("R9tMrXTQj3");
+  }
+}
---
+++ greetgo-cached-core/test_src/kz/greetgo/cached/core/DummyTest.java
@@ -0,0 +1,12 @@
+package kz.greetgo.cached.core;
+
+import org.testng.annotations.Test;
+
+public class DummyTest {
+
+  @Test
+  public void name() {
+    new Dummy().print();
+  }
+
+}
